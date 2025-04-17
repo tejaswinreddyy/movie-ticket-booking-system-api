5cdd775ff7e29abbc755d1026e9c1d8e7ba5d57d
Your Name <tejaswinreddy.y@gmail.com>
2025-04-17 16:53:15 +0530

    Init Commit | Project Initiated

---
+++ src/main/java/com/example/mtb/MovieTicketBookingSystemApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.mtb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketBookingSystemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketBookingSystemApiApplication.class, args);
+	}
+
+}
