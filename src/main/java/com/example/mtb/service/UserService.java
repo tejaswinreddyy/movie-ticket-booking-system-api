5cb0cdffd89c02101c24973f10fb53d77ebe3ad5
Your Name <tejaswinreddy.y@gmail.com>
2025-04-22 15:44:44 +0530

    Input DTO updating succesfully

--- src/main/java/com/example/mtb/service/UserService.java
+++ src/main/java/com/example/mtb/service/UserService.java
@@ -1,7 +1,8 @@
 package com.example.mtb.service;
 
+import com.example.mtb.dto.UserRegistrationRequest;
 import com.example.mtb.entity.UserDetails;
 
 public interface UserService {
-   public UserDetails addUser(UserDetails user);
+   public UserDetails addUser(UserRegistrationRequest user);
 }
