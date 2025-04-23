3aa55478fb7b13ee85e3b2f1e98406b97bcb3121
Your Name <tejaswinreddy.y@gmail.com>
2025-04-23 17:20:29 +0530

    Input Validation Resolved

--- src/main/java/com/example/mtb/dto/UserRegistrationRequest.java
+++ src/main/java/com/example/mtb/dto/UserRegistrationRequest.java
@@ -1,15 +1,35 @@
 package com.example.mtb.dto;
 
 import com.example.mtb.enums.UserRole;
+import jakarta.validation.constraints.*;
 
 import java.time.LocalDate;
 
-public record UserRegistrationRequest (
+public record UserRegistrationRequest(
+        @NotNull
+        @Pattern(regexp = "^(?=.{3,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z][a-zA-Z0-9._]*[a-zA-Z0-9]$",
+                message = "Username should be minimum of 3 and maximum of 20 alpha-numeric character and can have special characters . and _")
         String username,
+
+        @NotNull
+        @Email(message = "Invalid Email format")
         String email,
+
+        @NotNull
+        @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!]).{8,20}$",
+                message = "Password should be minimum of 8 characters and should have atleast one uppercase and lowercase alphabet, atleast one special character")
         String password,
+
+        @NotNull
+        @Pattern(regexp = "[6-9]\\d{9}$",
+                message = "Phone number must be a valid 10-digit Indian number starting with 6–9.")
         String phoneNumber,
+
+        @NotNull
         UserRole userRole,
+
+        @NotNull
+        @Past(message = "Date of birth must be a past date")
         LocalDate dateOfBirth
-)
-{}
+) {
+}
