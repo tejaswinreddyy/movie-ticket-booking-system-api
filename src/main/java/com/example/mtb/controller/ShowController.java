1a22a92cc7c5074f2883b694581644b90b9645ab
Your Name <tejaswinreddy.y@gmail.com>
2025-05-12 17:59:10 +0530

    No Changes

--- src/main/java/com/example/mtb/controller/ShowController.java
+++ src/main/java/com/example/mtb/controller/ShowController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
 @AllArgsConstructor
 @Validated
--- src/main/java/com/example/mtb/repository/TheaterRepository.java
+++ src/main/java/com/example/mtb/repository/TheaterRepository.java
@@ -1,7 +1,15 @@
 package com.example.mtb.repository;
 
 import com.example.mtb.entity.Theater;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.awt.print.Pageable;
+import java.time.Instant;
+import java.util.List;
 
 public interface TheaterRepository extends JpaRepository<Theater, String> {
 }
